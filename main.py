63f357d14bb20fe1fe901acb1819ed92889faa59
Triseedot <wdgsyq24dh@gmail.com>
2023-03-01 09:34:04 +0100

    Add webhook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, executor
-from aiogram.types import Message
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.executor import start_webhook
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
@@ -12,6 +11,25 @@ BOT_TOKEN = os.environ['BOT_TOKEN']
 bot = Bot(token=BOT_TOKEN)
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 CHANNEL_USERNAME = os.environ['CHANNEL_USERNAME']
 
@@ -32,4 +50,13 @@ async def checker(wait_for):
 
 if __name__ == '__main__':
     asyncio.ensure_future(checker(30))
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
